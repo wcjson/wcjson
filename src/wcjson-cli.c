c942ac900a293a94aaa1142c864e7782e85b403e
Christian Schulte <cs@schulte.it>
2025-04-23 01:39:26 +0200

    Stop serializing to memory when not reporting.

--- src/wcjson-cli.c
+++ src/wcjson-cli.c
@@ -336,13 +336,15 @@ int main(int argc, char *argv[]) {
 
   limit -= doc.e_nitems_cnt * sizeof(wchar_t);
 
-  size_t o_nitems = limit / sizeof(wchar_t);
-  if (o_nitems == 0) {
-    errno = ENOMEM;
-    goto err;
-  }
+  if (report) {
+    limit -= doc.e_nitems_cnt * sizeof(wchar_t);
 
-  if (report || o_nitems > len * WCJSON_ESCAPE_MAX) {
+    size_t o_nitems = limit / sizeof(wchar_t);
+    if (o_nitems == 0) {
+      errno = ENOMEM;
+      goto err;
+    }
+
     outb = malloc(o_nitems * sizeof(wchar_t));
     if (outb == NULL)
       goto err;
@@ -363,25 +365,17 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    if (report) {
-      total_bytes += o_nitems * sizeof(wchar_t);
-      fwprintf(stdout, L"Output characters: %lld\n", o_nitems);
-      fwprintf(stdout, L"Output characters (byte): %lld\n",
-               o_nitems * sizeof(wchar_t));
-    } else if (fputws(outb, out) == -1)
-      goto err;
+    total_bytes += o_nitems * sizeof(wchar_t);
+    fwprintf(stdout, L"Output characters: %lld\n", o_nitems);
+    fwprintf(stdout, L"Output characters (byte): %lld\n",
+             o_nitems * sizeof(wchar_t));
 
-  } else if (!report) {
-    if (ascii) {
-      if (wcjsondocfprintasc(out, &doc, doc.values) < 0)
-        goto err;
-    } else {
-      if (wcjsondocfprint(out, &doc, doc.values) < 0)
-        goto err;
-    }
+  } else if (ascii) {
+    if (wcjsondocfprintasc(out, &doc, doc.values) < 0)
+      goto err;
   } else {
-    fwprintf(stdout, L"Output characters: n/a\n");
-    fwprintf(stdout, L"Output characters (byte): n/a\n");
+    if (wcjsondocfprint(out, &doc, doc.values) < 0)
+      goto err;
   }
 
   if (ferror(out))
